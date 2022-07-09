632937da88e1fb5abc846a7d25bcce20c6d6aa2b
Ahmet Furkan Eren <ahmetfurkanerenn@gmail.com>
2022-07-09 21:01:32 +0300

    added files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 #### dotfiles
+
+
+###### Installation without cloning repo
+`wget https://raw.githubusercontent.com/aferen/dotfiles/master/install.sh -O -| bash`
+
+###### Installation
+`./setup.sh`
---
+++ setup.sh
@@ -0,0 +1,68 @@
+#!/bin/bash -eux
+
+sudo -v
+
+case "$(uname -s)" in
+    Darwin)
+        if [[ ! -x /usr/bin/gcc ]]; then
+          echo "[i] Install macOS Command Line Tools"
+          xcode-select --install
+        fi
+
+        if [[ ! -x /usr/local/bin/brew ]]; then
+          echo "[i] Install Homebrew"
+          /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        fi
+
+        if [[ ! -x /usr/local/bin/ansible ]]; then
+            echo "[i] Install Ansible"
+            brew install ansible
+        fi
+        ;;
+
+    Linux)
+        if [ -f /etc/os-release ]
+            then
+                . /etc/os-release
+
+                case "$ID" in
+                    debian | ubuntu)
+                        if [[ ! -x /usr/bin/ansible ]]; then
+                            echo "[i] Install Ansible"
+                            sudo apt-get install -y ansible
+                        fi
+                        ;;
+                    *)
+                        echo "[!] Unsupported Linux Distribution: $ID"
+                        exit 1
+                        ;;
+                esac
+            else
+                echo "[!] Unsupported Linux Distribution"
+                exit 1
+            fi
+        ;;
+
+    *)
+        echo "[!] Unsupported OS"
+        exit 1
+        ;;
+esac
+
+if [ -f "$HOME/.bashrc" ] && [ ! -h "$HOME/.bashrc" ]
+then
+    echo "[i] Move current ~/.bashrc to ~/bashrc_backup"
+    mv "$HOME/.bashrc" "$HOME/bashrc_backup"
+fi
+
+if [ -f "$HOME/.bash_profile" ] && [ ! -h "$HOME/.bash_profile" ]
+then
+    echo "[i] Move current ~/.bash_profile to ~/bash_profile_backup"
+    mv "$HOME/.bash_profile" "$HOME/bash_profile_backup"
+fi
+
+echo "[i] Run Playbook"
+ansible-playbook ../playbook.yml --verbose --ask-become-pass
+
+echo "[i] From now on you can use $ dotfiles to manage your dotfiles"
+echo "[i] Done."
